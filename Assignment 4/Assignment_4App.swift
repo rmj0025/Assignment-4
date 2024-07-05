540361a936b89385355ff50bbace430d5ee51cab
rmj0025 <127268170+rmj0025@users.noreply.github.com>
2024-07-04 22:29:40 -0500

    Initial Commit

---
+++ Assignment 4/Assignment_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_4App.swift
+//  Assignment 4
+//
+//  Created by Marion Jury on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
